d16aa5079c217551bd8862033f517cc48524af3c
zidanesb126 <zidanesb126@gmail.com>
2021-06-10 19:11:12 +0700

    Pengumpulan UAS

---
+++ Kelompok Web/Contributors.md
@@ -0,0 +1,4 @@
+Dibuat Oleh :
+    Zidane Saeful Budiman (A2.1900197)
+    Muhamad Hilmi (A2.1900114)
+    Ari Alhabsyi (A2.1900021)
---
+++ Kelompok Web/Database/db_toko.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 10 Jun 2021 pada 09.42
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_toko`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` varchar(255) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `nama_barang` text NOT NULL,
+  `merk` varchar(255) NOT NULL,
+  `harga_beli` varchar(255) NOT NULL,
+  `harga_jual` varchar(255) NOT NULL,
+  `satuan_barang` varchar(255) NOT NULL,
+  `stok` text NOT NULL,
+  `tgl_input` varchar(255) NOT NULL,
+  `tgl_update` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `id_kategori`, `nama_barang`, `merk`, `harga_beli`, `harga_jual`, `satuan_barang`, `stok`, `tgl_input`, `tgl_update`) VALUES
+('BR003', 0, 'Susu Beruang', 'BrenBrend', '5000', '6000', 'PCS', '100', '10 June 2021, 14:15', NULL),
+('BR004', 0, 'Susu Beruang', 'BrenBrend', '5800', '6000', 'PCS', '5', '10 June 2021, 14:16', NULL),
+('BR005', 0, 'sabun', 'lifeboy', '2000', '3000', 'PCS', '8', '10 June 2021, 14:17', NULL),
+('BR006', 7, 'Lifeboy', 'Lifeboy', '2000', '2500', 'PCS', '9', '10 June 2021, 14:21', NULL),
+('BR007', 8, 'Nabati', 'Nabati', '400', '500', 'PCS', '20', '10 June 2021, 14:22', NULL),
+('BR008', 9, 'Power F', 'Power F', '1000', '1500', 'PCS', '20', '10 June 2021, 14:26', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+(7, 'Sabun', '10 June 2021, 14:20'),
+(8, 'Makanan', '10 June 2021, 14:21'),
+(9, 'Minuman', '10 June 2021, 14:26');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `pass` char(32) NOT NULL,
+  `id_member` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`id_login`, `user`, `pass`, `id_member`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(11) NOT NULL,
+  `nm_member` varchar(255) NOT NULL,
+  `alamat_member` text NOT NULL,
+  `telepon` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gambar` text NOT NULL,
+  `NIK` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `member`
+--
+
+INSERT INTO `member` (`id_member`, `nm_member`, `alamat_member`, `telepon`, `email`, `gambar`, `NIK`) VALUES
+(1, 'Zidane Saeful Budiman', 'Paseh Kaler', '087771780304', 'zidanesb126@gmail.com', 'juventus-vs-genoa-ro_f9615c7.jpg', 'A2.1900197A2.1900114');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nota`
+--
+
+CREATE TABLE `nota` (
+  `id_nota` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL,
+  `periode` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `nota`
+--
+
+INSERT INTO `nota` (`id_nota`, `id_barang`, `id_member`, `jumlah`, `total`, `tanggal_input`, `periode`) VALUES
+(18, 'BR006', 1, '2', '5000', '10 June 2021, 14:24', '06-2021'),
+(19, 'BR006', 1, '2', '5000', '10 June 2021, 14:24', '06-2021'),
+(20, 'BR006', 1, '7', '17500', '10 June 2021, 14:24', '06-2021'),
+(22, 'BR006', 1, '7', '17500', '10 June 2021, 14:24', '06-2021'),
+(24, 'BR007', 1, '50', '25000', '10 June 2021, 14:28', '06-2021'),
+(25, 'BR006', 1, '1', '2500', '10 June 2021, 14:29', '06-2021');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_barang`, `id_member`, `jumlah`, `total`, `tanggal_input`) VALUES
+(13, 'BR006', 1, '1', '2500', '10 June 2021, 14:29');
+
+--
+-- Trigger `penjualan`
+--
+DELIMITER $$
+CREATE TRIGGER `edit` AFTER UPDATE ON `penjualan` FOR EACH ROW BEGIN
+	UPDATE barang SET stok = stok-NEW.jumlah + OLD.jumlah
+    where id_barang = NEW.id_barang;
+ END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `toko`
+--
+
+CREATE TABLE `toko` (
+  `id_toko` int(11) NOT NULL,
+  `nama_toko` varchar(255) NOT NULL,
+  `alamat_toko` text NOT NULL,
+  `tlp` varchar(255) NOT NULL,
+  `nama_pemilik` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `toko`
+--
+
+INSERT INTO `toko` (`id_toko`, `nama_toko`, `alamat_toko`, `tlp`, `nama_pemilik`) VALUES
+(1, 'TOKO UAS', 'Jl. Raden Ali Sadikin, RT 019/008, Kab Sumedang', '087771780304', 'Zidane Saeful Budiman');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indeks untuk tabel `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indeks untuk tabel `nota`
+--
+ALTER TABLE `nota`
+  ADD PRIMARY KEY (`id_nota`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`);
+
+--
+-- Indeks untuk tabel `toko`
+--
+ALTER TABLE `toko`
+  ADD PRIMARY KEY (`id_toko`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `nota`
+--
+ALTER TABLE `nota`
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `toko`
+--
+ALTER TABLE `toko`
+  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
